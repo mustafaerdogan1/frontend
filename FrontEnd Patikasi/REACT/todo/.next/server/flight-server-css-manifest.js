354c8bb98489c022bf73615686bca4d692838dcc
Mustafa ERDOĞAN <mustafa.erdogan7@outlook.com>
2023-03-22 16:06:15 +0300

    ödev

---
+++ FrontEnd Patikasi/REACT/todo/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "C:\\Users\\PC\\Desktop\\New folder\\todo\\app\\page": [
+      "C:\\Users\\PC\\Desktop\\New folder\\todo\\styles\\globals.css"
+    ]
+  },
+  "C:\\Users\\PC\\Desktop\\New folder\\todo\\app\\page.jsx": [
+    "C:\\Users\\PC\\Desktop\\New folder\\todo\\styles\\globals.css"
+  ]
+}
---
+++ FrontEnd Patikasi/REACT/todo/app/page.jsx
@@ -0,0 +1,12 @@
+import "@/styles/globals.css"
+import { TodoWrapper } from './components/TodoWrapper';
+
+function App() {
+    return (
+        <div className="App">
+            <TodoWrapper />
+        </div>
+    );
+}
+
+export default App;
